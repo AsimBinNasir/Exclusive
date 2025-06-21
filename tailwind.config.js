0d48c676d963982c7f8b67b02a61d591720b1fe0
AsimBinNasir <asimbinnasir2906@email.com>
2025-06-22 04:51:28 +0600

    added header, and created categories

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,37 @@
-module.exports = {
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx,html}",
   ],
-  theme: { extend: {} },
-  plugins: [],
+  theme: {
+    extend: {
+      fontSize: {
+        'xxs': '0.625rem', // 10px
+      }
+    },
+  },
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        '.underline-slide': {
+          position: 'relative',
+          '&::after': {
+            content: "''",
+            position: 'absolute',
+            left: '0',
+            bottom: '0',
+            width: '0%',
+            height: '2px',
+            backgroundColor: 'currentColor',
+            transition: 'width 0.3s ease-in-out',
+          },
+          '&:hover::after': {
+            width: '100%',
+          },
+        },
+      });
+    },
+  ],
 }
